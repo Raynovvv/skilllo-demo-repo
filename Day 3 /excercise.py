97c317602e7ebf72d36ab41331d7b52cb6b48464
chris <krainov234@Gmail.com>
2024-04-22 18:46:49 +0300

    Day 3 - excercise.py

--- Day 3 /excercise.py
+++ Day 3 /excercise.py
@@ -1,101 +1,24 @@
-# Exercise 1: Online Shopping Discount
-
-# Given a variable `total_amount` representing the total amount in the shopping cart,
-# write a program that prints a discount message if the total amount is over $100,
-# and always prints a 'thank you' message.
-
-total_amount = 120  # Fill in the total amount here
+# Problem 0:
+# Write a program that takes an integer input from the user
+# and checks if it's odd or even. Use an if-else statement to print the result.
+import random
 
 
-def first(total_amount):
-    if total_amount >= 120:
-        print('You have discount')
-    print('Thank you!')
-
-
-# Exercise 2: Temperature Checker
-
-# Given a variable `temperature` representing the current temperature in Celsius,
-# write a program that prints "Warm" if the temperature is greater than or equal to 25 degrees Celsius,
-# otherwise print "Cool".
-
-temperature = 22  # Fill in the temperature here
-
-
-def second(temp):
-    if temp >= 25:
-        print('Warm\n')
+def problem_0():
+    input_number = input("Give me a number:\n")
+    if (int(input_number) % 2) == 0:
+        print(f"{input_number} is even")
     else:
-        print('Cool\n')
+        print(f"{input_number} is odd")
 
 
-...
-
-# Exercise 3: Time of the Day
-
-# Given a variable `hour` representing the current hour (in 24-hour format),
-# write a program that prints "Good Morning" if the hour is before 12 (12 inclusive),
-# "Good Afternoon" if the hour is between 12 and 17 (17 inclusive),
-# and "Good Evening" if the hour is after 17.
-
-hour = 15  # Fill in the hour here
+# problem_0()
 
 
-def third(hour):
-    if 5 < hour < 12:
-        print('Good Morning!')
-    elif 12 < hour < 17:
-        print()
-
-
-...
-
-
-# Exercise 4: Secret Message
-
-# Given a variable `message` representing a secret message,
-# write a program that prints "Message found" if the message is not empty,
-# otherwise print "No message".
-
-def fourth():
-    message = ""  # Fill in the message here
-    if message:
-        print("Message found")
-    else:
-        print("No message")
-
-
-...
-
-
-# Exercise 5: List Iteration
-
-# You have a list of fruits. Write a program that iterates over each fruit in the list and prints each fruit's name.
-def fifth():
-    fruits = ["Apple", "Banana", "Orange", "Grape", "Watermelon"]
-    for x in fruits:
-        print(x)
-
-
-...
-
-
-# Exercise 6: Range Iteration
-
-# Write a program that prints all the even numbers from 1 to 20 using a for loop and the range() function.
-def sixth():
-    numbers = range(1, 21)
-    for x in numbers:
-        print(x)
-
-
-...
-
-
-# Exercise 7: While Loop
-
-# Write a program using a while loop to find the sum of all numbers from 1 to 100.
-def seventh():
+# Write a Python program to find the sum of all even numbers from
+# 1 to 100 using a loop. Make use of control flow constructs like
+# the for loop and conditional statements.
+def problem_1():
     i = 1
     sum = 0
     while i <= 100:
@@ -104,87 +27,132 @@ def seventh():
     print(sum)
 
 
-...
+# problem_1()
 
-# Exercise 8: Number of Friends
+# Problem 2:
+# Write a Python script that prompts the user in the console a simple problem
+# ( how much does 5 + 17 equal to ) until the user provides a correct answer.
 
-# Write a program that asks how many friends you have and then asks for each of their names.
-
-num_friends = 2
-
-
-def eight(friends):
-    for x in range(friends):
-        input("Enter a name for your friend:\n")
-
-
-# Exercise 9: Guess the Number
-# Write a program that has a number and keeps asking the user to input a number until the user guesses it.
-
-secret_number = 42
+def problem_2():
+    num_1 = random.randrange(1, 100)
+    num_2 = random.randrange(1, 100)
+    print(f"Whats the result of {num_1} + {num_2} ?\n")
+    sum_of_nums = int(num_1) + int(num_2)
+    user_input = int(input())
+    while user_input != sum_of_nums:
+        print(f"{user_input} is not correct answer. Try again : \n")
+        user_input = input()
+    print(f"{user_input} is correct!")
 
 
-def ninth(number):
-    guess = input("Enter a number:\n")
-    while not int(number) == int(guess):
-        guess = input("Try again:\n")
-    print("You guessed it!")
+# problem_2()
+
+# Problem 3:
+# Write a Python script that iterates over the first 1000 numbers and prints "Fizz" if the number is divisible by 3,
+# "Buzz" if it's divisible by 5, and "FizzBuzz" if it's divisible by both 3 and 5.
+
+def problem_3():
+    numbers = range(1, 1000)
+    for num in numbers:
+        if int(num) % 3 and int(num) % 5:
+            print("FizzBuzz")
+        elif num % 5:
+            print("Buzz")
+        elif num % 3:
+            print("Fizz")
 
 
-...
+# problem_3()
 
+# Problem 4:
+# Design a Python program that simulates a simple guessing game.
+# The program should generate a random number between 1 and 100 and ask the user to guess it.
+# Provide hints like "Too high" or "Too low" until the user guesses the correct number. Use a while loop for this game.
 
-# Exercise 10: Multiplication Table
-
-# Generate the multiplication table for the numbers 1 to 9.
-
-def tenth():
-    nums_to_multiply = range(1, 11)
-    for x in nums_to_multiply:
-        for i in nums_to_multiply:
-            print(i * x)
-
-
-# Exercise 11: continue
-
-# Do the same as in Exercise 6 but do not print the number 10. Use `continue` do achieve this
-def eleventh():
-    numbers = range(1, 21)
-    for x in numbers:
-        if int(x) == 10:
-            continue
-        print(x)
-
-
-...
-
-# Exercise 12: Password Checker
-
-# Write a program that asks the user to enter a password. If the password is correct, print a message saying
-# "Access granted" and end the program. If the user enters the wrong password three times, print "Access denied" and
-# end the program..
-
-correct_password = "learnpythonwithskillo"
-
-
-def twelfth(password):
-    i = 0
+def problem_4():
+    random_num = random.randrange(1, 100)
+    user_input = int(input("Enter a number:\n"))
     while True:
-        user_input = input("Enter your password:\n")
-        i += 1
-        if user_input == password:
-            print("Access granted")
-            break
-        if user_input != password and i >= 3:
-            print("Access denied")
+        if user_input < random_num:
+            print("Too low")
+            user_input = int(input("Try again:\n"))
+        elif user_input > random_num:
+            print("Too high")
+            user_input = int(input("Try again:\n"))
+        else:
+            print("Got it!")
             break
 
 
-...
-#first(total_amount)
-#second(temperature)
-#eight(num_friends)
-#ninth(secret_number)
-#eleventh()
-#twelfth(correct_password)
-#tenth()
+# Problem 5:
+# Modify problem 2 so that every time the user is prompted the problem is different.
+# Think of a way to design that and come up with a proper solution for that.
+
+def problem_5():
+    num_1 = random.randrange(1, 100)
+    num_2 = random.randrange(1, 100)
+    print(f"Whats the result of {num_1} + {num_2} ?\n")
+    sum_of_nums = int(num_1) + int(num_2)
+    user_input = int(input())
+    while user_input != sum_of_nums:
+        print(f"{user_input} is not correct answer. Try again : \n")
+        num_1 = random.randrange(1, 100)
+        num_2 = random.randrange(1, 100)
+        print(f"Whats the result of {num_1} + {num_2} ?\n")
+        sum_of_nums = int(num_1) + int(num_2)
+        user_input = int(input())
+    print(f"{user_input} is correct!")
+
+
+# problem_5()
+
+# Problem 6:
+# Write a Python program that takes an integer input from the user and
+# prints the multiplication table for that number from 1 to 10 using a for loop.
+
+def problem_6():
+    user_input = int(input("Give me a number:\n"))
+    range_of_nums = range(1, 11)
+    for x in range_of_nums:
+        print(user_input * int(x))
+
+
+# problem_6()
+
+# Problem 7:
+# Create a Python program that checks if a given integer is a prime number.
+# Use a for loop to iterate through possible divisors and use an if-else statement to determine if it's prime.
+
+def problem_7():
+    num = int(input("Give me an number:\n"))
+    if num == 1:
+        print(num, "is not a prime number")
+    elif num > 1:
+        # check for factors
+        for i in range(2, num):
+            if (num % i) == 0:
+                print(num, "is not a prime number")
+                print(i, "times", num // i, "is", num)
+                break
+        else:
+            print(num, "is a prime number")
+
+    # if input number is less than
+    # or equal to 1, it is not prime
+    else:
+        print(num, "is not a prime number")
+
+
+# problem_7()
+
+# Problem 8: Pattern Printing
+# Write a program that takes an integer 'n' as input and prints the following pattern using nested for loops:
+
+def problem_8():
+    input_num = int(input("Give me a num:\n"))
+    for i in range(1, input_num + 1):
+        for j in range(1, i + 1):
+            print(j, end="")
+        print()
+
+problem_8()
